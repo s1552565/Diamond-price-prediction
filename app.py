95de765cb6faad67546596b37e3dd342d4ef770e
s1552565 <suryashekhawat52@gmail.com>
2024-01-29 15:29:10 +0530

    app.py and application

--- app.py
+++ app.py
@@ -38,10 +38,3 @@ def predict_datapoint():
 
         return render_template('results.html',final_result=results)
 
-
-
-
-
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True)
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages,setup
-from typing import List
+
 
 HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path:str):
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
